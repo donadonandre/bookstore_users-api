be39f1f23ffcda6fabe79507affb6ba462f32cf6
André Donadon <andreltdonadon@gmail.com>
2023-04-18 22:32:31 -0300

    8 - dao structure

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -6,10 +6,24 @@ import (
 	"bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func GetUser(c *gin.Context) {
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
 
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
 func CreateUser(c *gin.Context) {
